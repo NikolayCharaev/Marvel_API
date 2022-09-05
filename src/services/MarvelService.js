3da174cd9cb2efbac8c805b4a1faa4d887129173
Good_Cthulhu <hayyabusa_01@mail.ru>
2022-09-05 18:59:04 +0300

    оптимизация получения данных и создание отображения случайного персонажа

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,3 @@
-
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   _apiKey = 'apikey=7dc0b21564efe32c71bf869268dd6098';
@@ -9,12 +8,24 @@ class MarvelService {
     }
     return await res.json();
   };
-  getAllCharacter = () => {
-    return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  getAllCharacter = async () => {
+    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter)
   };
 
-  getCharacter = (id) => {
-    return this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+  getCharacter = async (id) => {
+    const res = await this.getResourse(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0])
+  };
+
+  _transformCharacter = (res) => {
+    return {
+      name: res.name,
+      description: res.description ? res.description.slice(0,100) + '...' : 'not description :(',
+      thumbnail: res.thumbnail.path + '.' + res.thumbnail.extension,
+      homepage: res.urls[0].url,
+      wiki: res.urls[1].url,
+    };
   };
 }
 
